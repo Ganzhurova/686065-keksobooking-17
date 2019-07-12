3882b9a5a67c9a73249ad65894c2562455078650
Darima Ganzhurova <DGanzhurova@gmail.com>
2019-07-13 01:12:17 +0900

    Личный проект: перламутровые пуговицы. Часть 2

--- js/map.js
+++ js/map.js
@@ -2,7 +2,8 @@
 
 (function () {
   var cards = [];
-  var housingType;
+
+  var housing = window.housingFilter.renderValue();
 
   var pinsListEl = document.querySelector('.map__pins');
   var pinMain = pinsListEl.querySelector('.map__pin--main');
@@ -44,31 +45,92 @@
     }
   };
 
+  var getPriceOption = function (price) {
+    if (price < 10000) {
+      var option = 'low';
+    } else if (price > 50000) {
+      option = 'high';
+    } else {
+      option = 'middle';
+    }
+
+    return option;
+  };
+
+  var getMaxRank = function () {
+    var maxRank = 0;
+
+    for (var key in housing) {
+      if (housing.hasOwnProperty(key)) {
+        maxRank++;
+      }
+    }
+
+    return maxRank;
+  };
+
+  var getCardRank = function (card) {
+    var rank = 0;
+    var priceCard = card.offer.price;
+    card.offer.price = getPriceOption(card.offer.price);
+
+    for (var key in housing) {
+      if (housing.hasOwnProperty(key)) {
+        var filterValue = housing[key];
+        var cardValue = String(card.offer[key]);
+
+        if (filterValue === 'any') {
+          rank++;
+        } else if (filterValue === cardValue) {
+          rank++;
+        } else if (cardValue === 'undefined') {
+          for (var i = 0; i < card.offer.features.length; i++) {
+            if (filterValue === card.offer.features[i]) {
+              rank++;
+            }
+          }
+        }
+      }
+    }
+
+    card.offer.price = priceCard;
+    return rank;
+  };
+
   var updatePins = function () {
     window.card.remove();
 
-    if (housingType === 'any') {
-      window.pin.render(cards);
-      addClickkHandler(cards);
-    } else {
-      var newCards = cards.filter(function (card) {
-        return card.offer.type === housingType;
-      });
-      window.pin.render(newCards);
-      addClickkHandler(newCards);
-    }
+    var newCards = cards.filter(function (card) {
+      var maxRank = getMaxRank();
+      var cardRank = getCardRank(card);
+
+      return cardRank === maxRank;
+    });
+
+    window.pin.render(newCards);
+    addClickkHandler(newCards);
   };
 
-  window.mapFilter.onHousingTypeChange = function (newHousingType) {
-    housingType = newHousingType;
-    updatePins();
+  var addOnChange = function (key) {
+    var filter = window.housingFilter[key];
+    filter.onChange = function (newValue) {
+      housing[key] = newValue;
+      updatePins();
+    };
+  };
+
+  var addChangeHandler = function () {
+    for (var key in window.housingFilter) {
+      if (window.housingFilter.hasOwnProperty(key)) {
+        addOnChange(key);
+      }
+    }
   };
 
   var successfulLoadHandler = function (data) {
     cards = data;
 
-    window.pin.render(cards);
-    addClickkHandler(cards);
+    updatePins();
   };
 
   var errorHandler = function () {
@@ -79,6 +141,7 @@
     if (window.pageMode.isBlocked()) {
       window.pageMode.active();
       window.backend.load(successfulLoadHandler, errorHandler);
+      addChangeHandler();
     }
   });
 })();
